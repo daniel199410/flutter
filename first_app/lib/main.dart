3ed7b68e4b99ca19f7467169e022f4d3a2818ca6
Daniel Cataño Restrepo <dcatanor@unal.edu.co>
2019-11-12 11:02:47 -0500

    Arrow functionr efactor

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -2,14 +2,9 @@ import 'package:flutter/material.dart';
 
 double addNumbers(double num1, double num2) => num1 + num2;
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(home: Text('Material App'),);
-  }
-
+  Widget build(BuildContext context) => MaterialApp(home: Text('Material App'),);
 }
