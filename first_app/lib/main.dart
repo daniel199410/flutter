faa03140eb7cea91b885fedbcbe069c2e6fa9ffe
Daniel Cataño Restrepo <dcatanor@unal.edu.co>
2019-11-12 11:50:25 -0500

    Column layout

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -5,13 +5,31 @@ double addNumbers(double num1, double num2) => num1 + num2;
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  void answerQuestion() => print('q');
+
   @override
   Widget build(BuildContext context) => MaterialApp(
         home: Scaffold(
           appBar: AppBar(
             title: Text('Material app'),
           ),
-          body: Text('Body'),
+          body: Column(
+            children: [
+              Text('The question'),
+              RaisedButton(
+                child: Text('answer1'),
+                onPressed: answerQuestion,
+              ),
+              RaisedButton(
+                child: Text('answer2'),
+                onPressed: () => null,
+              ),
+              RaisedButton(
+                child: Text('answer3'),
+                onPressed: () => null,
+              ),
+            ],
+          ),
         ),
       );
 }
